ff8245a0eb75da6619e5bb5f0b3c84af571b3267
Sebastian Lech <lechsebastian99@gmail.com>
2024-09-13 01:33:20 +0200

    Implement adding restaurants

--- lib/app/home/add_opinion/add_opinion_page_content.dart
+++ lib/app/home/add_opinion/add_opinion_page_content.dart
@@ -1,14 +1,59 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class AddOpinionPageContent extends StatelessWidget {
+class AddOpinionPageContent extends StatefulWidget {
   const AddOpinionPageContent({
     super.key,
   });
 
   @override
+  State<AddOpinionPageContent> createState() => _AddOpinionPageContentState();
+}
+
+class _AddOpinionPageContentState extends State<AddOpinionPageContent> {
+  var restaurantName = '';
+  var pizzaName = '';
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('2'),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextField(
+            onChanged: (value) {
+              setState(() {
+                restaurantName = value;
+              });
+            },
+            decoration: const InputDecoration(
+              label: Text('Restauracja'),
+            ),
+          ),
+          const SizedBox(height: 20),
+          TextField(
+            onChanged: (value) {
+              setState(() {
+                pizzaName = value;
+              });
+            },
+            decoration: const InputDecoration(
+              label: Text('Pizza'),
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              FirebaseFirestore.instance.collection('restaurants').add({
+                'name': restaurantName,
+                'pizza': pizzaName,
+                'rating': 3.0,
+              });
+            },
+            child: const Text('Dodaj'),
+          ),
+        ],
+      ),
     );
   }
 }

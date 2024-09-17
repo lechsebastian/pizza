6f8f08ba7757acfbc3db9187d438b91b55173f52
Sebastian Lech <lechsebastian99@gmail.com>
2024-09-17 23:12:35 +0200

    Move home and login into features folder

--- lib/app/features/home/home_page.dart
+++ lib/app/features/home/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:pizza/app/home/add_opinion/add_opinion_page_content.dart';
-import 'package:pizza/app/home/my_account/my_account_page_content.dart';
-import 'package:pizza/app/home/restaurants/restaurants_page_content.dart';
+import 'package:pizza/app/features/home/add_opinion/add_opinion_page_content.dart';
+import 'package:pizza/app/features/home/my_account/my_account_page_content.dart';
+import 'package:pizza/app/features/home/restaurants/restaurants_page_content.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:pizza/app/home/home_page.dart';
-import 'package:pizza/app/login/login_page.dart';
+import 'package:pizza/app/features/home/home_page.dart';
+import 'package:pizza/app/features/login/login_page.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
